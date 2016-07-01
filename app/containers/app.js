8b5db59e17b8a14f26f054fd9426fc3f39ec0e4c
jiyinyiyong <jiyinyiyong@gmail.com>
2016-07-01 10:48:20 +0800

    putting Router directly under Provider

--- app/containers/app.js
+++ app/containers/app.js
@@ -4,19 +4,23 @@ import {View, Text, StyleSheet} from 'react-native';
 import List from '../containers/list';
 import Detail from '../containers/detail';
 import {Router, Scene} from 'react-native-router-flux';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from '../reducers/';
+
+var store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>{'App'}</Text>
+      <Provider store={store}>
         <Router>
           <Scene key="root">
             <Scene key="list" component={List} title="List" hideNavBar={true} initial={true} />
             <Scene key="detail" component={Detail} hideNavBar={true} title="Detail" />
           </Scene>
         </Router>
-      </View>
+      </Provider>
     );
   }
 }
--- index.android.js
+++ index.android.js
@@ -12,20 +12,5 @@ import {
   View
 } from 'react-native';
 import App from './app/containers/app';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducer from './app/reducers/';
 
-var store = createStore(reducer);
-
-class Main extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  }
-}
-
-AppRegistry.registerComponent('templateReduxRN', () => Main);
+AppRegistry.registerComponent('templateReduxRN', () => App);
